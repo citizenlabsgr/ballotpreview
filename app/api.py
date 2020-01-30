a029ed5e934bd9b9087369e7bff9002fb906f8f7
Jace Browning <jacebrowning@gmail.com>
2020-01-30 14:34:59 -0500

    Block invalid date formats

--- app/api.py
+++ app/api.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Tuple
 
 import aiohttp
+import bugsnag
 
 
 BASE_URL = "https://michiganelections.io/api"
@@ -10,17 +11,21 @@ async def get_status(
     first_name: str, last_name: str, birth_date: str, zip_code: int
 ) -> Dict:
     async with aiohttp.ClientSession() as session:
-        async with session.get(
-            f"{BASE_URL}/registrations/?first_name={first_name}&last_name={last_name}&birth_date={birth_date}&zip_code={zip_code}"
-        ) as response:
+        url = f"{BASE_URL}/registrations/?first_name={first_name}&last_name={last_name}&birth_date={birth_date}&zip_code={zip_code}"
+        async with session.get(url) as response:
             data = await response.json()
+
+    if "registered" not in data:
+        bugsnag.notify(ValueError(f"Invalid response: {url}"))
+        data = {"registered": False}
 
     return data
 
 
 async def get_elections() -> List:
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"{BASE_URL}/elections/?limit=1000") as response:
+        url = f"{BASE_URL}/elections/"
+        async with session.get(url) as response:
             data = await response.json()
 
     return data["results"]
@@ -28,7 +33,8 @@ async def get_elections() -> List:
 
 async def get_election(election_id: int) -> Dict:
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"{BASE_URL}/elections/{election_id}/") as response:
+        url = f"{BASE_URL}/elections/{election_id}/"
+        async with session.get(url) as response:
             data = await response.json()
 
     return data
@@ -36,9 +42,8 @@ async def get_election(election_id: int) -> Dict:
 
 async def get_ballot(election_id: int, precinct_id: int) -> Tuple[Dict, List, List]:
     async with aiohttp.ClientSession() as session:
-        async with session.get(
-            f"{BASE_URL}/ballots/?election_id={election_id}&precinct_id={precinct_id}&active_election=null"
-        ) as response:
+        url = f"{BASE_URL}/ballots/?election_id={election_id}&precinct_id={precinct_id}&active_election=null"
+        async with session.get(url) as response:
             data = await response.json()
             try:
                 ballot = data["results"][0]
@@ -46,15 +51,13 @@ async def get_ballot(election_id: int, precinct_id: int) -> Tuple[Dict, List, Li
                 ballot = None
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(
-            f"{BASE_URL}/positions/?election_id={election_id}&precinct_id={precinct_id}&active_election=null&limit=1000"
-        ) as response:
+        url = f"{BASE_URL}/positions/?election_id={election_id}&precinct_id={precinct_id}&active_election=null&limit=1000"
+        async with session.get(url) as response:
             positions = await response.json()
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(
-            f"{BASE_URL}/proposals/?election_id={election_id}&precinct_id={precinct_id}&active_election=null&limit=1000"
-        ) as response:
+        url = f"{BASE_URL}/proposals/?election_id={election_id}&precinct_id={precinct_id}&active_election=null&limit=1000"
+        async with session.get(url) as response:
             proposals = await response.json()
 
     return ballot, positions["results"], proposals["results"]
